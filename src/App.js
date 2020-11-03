97d8adb150baae4ca9053fb0d9c0eecf64c098c6
llyasstrh <ilyasstrh@gmail.com>
2020-11-03 15:23:52 +0100

    add reCaptcha

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Hero from './components/Hero';
 import Contact from './components/Contact';
@@ -10,67 +10,68 @@ import Skills from './components/Skills';
 import ScrollToTop from './components/ScrollToTop';
 import ParticlesBg from 'particles-bg'
 import Particles from 'react-particles-js';
-
+import { ToastContainer, toast } from 'react-toastify';
 
 
 
 function App() {
-  return (
-    <div>
-      <ScrollToTop />
-      <Navbar />
-      <Particles
-        style={{
-          position: 'absolute',
-          opacity: "0.3",
-        }}
-        params={{
-          particles: {
-            number: {
-              value: 45,
-              density: {
-                enable: true,
-                value_area: 850,
+    return (
+      <div>
+        <ScrollToTop />
+        <Navbar />
+        <Particles
+          style={{
+            position: 'absolute',
+            opacity: "0.3",
+          }}
+          params={{
+            particles: {
+              number: {
+                value: 45,
+                density: {
+                  enable: true,
+                  value_area: 850,
+                },
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 1,
+                },
+              },
+              size: {
+                value: 5,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 4,
+                  size_min: 0.1,
+                  sync: true,
+                },
+              },
+              opacity: {
+                value: 1,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0.3,
+                  sync: true,
+                },
               },
             },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 1,
-              },
-            },
-            size: {
-              value: 5,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 4,
-                size_min: 0.1,
-                sync: true,
-              },
-            },
-            opacity: {
-              value: 1,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 1,
-                opacity_min: 0.3,
-                sync: true,
-              },
-            },
-          },
-        }}
-      />
-      
-      <Hero />
-      <Education />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  );
-}
+          }}
+        />
+
+        <Hero />
+        <Education />
+        <Skills />
+        <Projects />
+        <ToastContainer />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,88 @@
-import react from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ReCAPTCHA from "react-google-recaptcha";
+
 
 function Contact() {
+    const recaptchaRef = React.createRef();
+    const FORM_API = process.env.FORM_ENDPOINT; //from formspree.io
+    const [form, setForm] = useState({});
+    const reCaptcha = useRef();
+    const [token, setToken] = useState("");
+    const notifySuccess = () => {
+        toast.success('Message sent successfully, Thanks!', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+    const notifyError = () => {
+        toast.error('Ooops! There was an error', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+    const updateForm = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const submitForm = (ev) => {
+        ev.preventDefault();
+        if (!token) {
+            toast.error('You must verify the captcha', {
+                position: "top-center",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+            });
+            return;
+        }
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                form.reset();
+                notifySuccess();
+                console.log("success");
+                reCaptcha.current.reset();
+                setToken("");
+
+            } else {
+                notifyError();
+                console.log("error");
+                reCaptcha.current.reset();
+                setToken("");
+            }
+        };
+        xhr.send(data);
+        setForm({
+            name: '',
+            email: '',
+            message: '',
+        })
+    }
+
     return (
         <section id="contact-me">
             <div className="contact">
@@ -24,17 +105,25 @@ function Contact() {
                     </ul>
                 </div>
                 <div className="contact-form">
-                    <form>
+                    <form onSubmit={submitForm}
+                        action="https://formspree.io/mvovrqdy"
+                        method="POST">
                         <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Your name*" />
+                            <input type="text" name="name" onChange={updateForm} value={form.name} className="form-control" placeholder="Your name*" required />
                         </div>
                         <div className="form-group">
-                            <input type="email" className="form-control" placeholder="Your email*" />
+                            <input type="email" name="email" onChange={updateForm} value={form.email} className="form-control" placeholder="Your email*" required />
                         </div>
                         <div className="form-group">
-                            <textarea placeholder="Message*" className="form-control" rows="4"></textarea>
+                            <textarea placeholder="Message*" name="message" onChange={updateForm} value={form.message} className="form-control" rows="4" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></textarea>
                         </div>
                         <button style={{ background: "linear-gradient(180deg, #1e3c72, #2a5298)", color: "white" }} type="submit" className="btn">Submit message</button>
+                        <ReCAPTCHA
+                            ref={reCaptcha}
+                            sitekey="6LdbNd4ZAAAAAOSnmQXmHrw8PBhV5vVzGE-JZ421"
+                            onChange={token => setToken(token)} 
+                            onExpired={e => setToken("")}
+                        />
                     </form>
                 </div>
             </div>
